63a919aa2ce2a574f9cb7fff1411dc57a1cc5108
John Hinrichs <jmhinric@gmail.com>
2014-02-22 18:14:10 -0500

    Problem 12 notes

--- problems/problem12.rb
+++ problems/problem12.rb
@@ -2,13 +2,21 @@ def triangle_number(number)
   number * (number + 1) / 2
 end
 
+# Version that ran for over an hour and only reached i ~ 5000 (answer is at i = 12,375)
+# def number_of_factors(number)
+#   factors = (1..number).select {|n| number % n == 0}
+#   return factors.count
+# end
+
+# Exponentially faster version! Took 25 seconds!
 def number_of_factors(number)
   sqrt_num = Math.sqrt(number)
-  factors = (1..sqrt_num).select {|n| number % n == 0}.length
+  factors = (1..sqrt_num).select {|n| number % n == 0}
+  number_of_factors = factors.count
   if number % sqrt_num == 0
-    return factors * 2 - 1
+    return number_of_factors * 2 - 1
   else
-    return factors * 2
+    return number_of_factors * 2
   end
 end
 
@@ -16,6 +24,7 @@ def get_answer(range)
   i = 1
   
   while number_of_factors(triangle_number(i)) < range
+    print Time.now.strftime("%l:%M:%S:%L ")
     puts "#{i}: triangle(#{i}): #{triangle_number(i)}, factors: #{number_of_factors(triangle_number(i))}"
     i += 1
   end 
