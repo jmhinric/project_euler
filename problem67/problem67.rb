2c1954b9431b5171e17c0a007ce85b6d122c30fa
John Hinrichs <jmhinric@gmail.com>
2014-04-20 23:18:48 -0400

    refactor problem 67

--- problem67/problem67.rb
+++ problem67/problem67.rb
@@ -3,12 +3,21 @@ require 'csv'
 
 data = CSV.read('data.csv', { :col_sep => ' ' })
 
+# Change each element's data type from string to integer
+data = data.map do |arr|
+  arr.map { |num| num.to_i }
+end
+
+# Find the max sum
+
 i = data.length - 2
+
+# 
 while i >= 0
   data[i].each_with_index do |num, index|
-    # binding.pry
-    data[i][index] = num.to_i + data[i+1][index].to_i > num.to_i + data[i+1][index+1].to_i ? num.to_i + data[i+1][index].to_i : num.to_i + data[i+1][index+1].to_i
-    # binding.pry
+    sum1 = num + data[i + 1][index]
+    sum2 = num + data[i + 1][index + 1]
+    data[i][index] = sum1 > sum2 ? sum1 : sum2
   end
   i -= 1
 end
