caeaf08feb48bbf14f7e38d1bed42735ef1393dc
John Hinrichs <jmhinric@gmail.com>
2014-05-23 23:32:09 -0400

    problem 26

---
+++ problem26.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+def longest_cycle
+  cycles = []
+
+  i = 2
+
+  while i < 1000
+    decimals = (1 / i.to_f).to_s.split(".")
+    cycle = 1
+    decimals.shift.each_with_index do |dec|
+
+    end
+    i += 1
+  end
+
+end
+
+longest_cycle
